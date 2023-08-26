903e9b49a4fbcba98acacdf82ed367e653e9688a
jh-lee99 <113870771+jh-lee99@users.noreply.github.com>
2023-08-26 19:52:49 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ from langchain.chains import RetrievalQA
 import streamlit as st
 import tempfile
 import os
+from streamlit_extras.buy_me_a_coffee import button
+
+button(username="jun99h1219t", floating=True, width=221)
 
 # 제목
 st.title("ChatPDF")
 st.write("---")
+
+# OpenAI KEY 입력 받기
+openai_key = st.text_input('OPEN_AI_API_KEY', type="password")
 
 # 파일 업로드
 uploaded_file = st.file_uploader("PDF 파일을 올려주세요", type=['pdf'])
@@ -32,7 +38,7 @@ if uploaded_file is not None:
     # Split
     text_splitter = RecursiveCharacterTextSplitter(
         # Set a really small chunk size, just to show.
-        chunk_size = 100,
+        chunk_size = 300,
         chunk_overlap  = 20,
         length_function = len,
         is_separator_regex = False,
@@ -40,7 +46,7 @@ if uploaded_file is not None:
     texts = text_splitter.split_documents(pages)
 
     # Embedding
-    embeddings_model = OpenAIEmbeddings()
+    embeddings_model = OpenAIEmbeddings(openai_api_key=openai_key)
 
     # load it into Chroma
     db = Chroma.from_documents(texts, embeddings_model)
@@ -49,8 +55,8 @@ if uploaded_file is not None:
     st.header("PDF에게 질문해보세요!!")
     question = st.text_input("질문을 입력하세요")
     if st.button("제출"):
-        with st.spinner('Wait for it...'):
-            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+        with st.spinner('쪼매 기다리시오'):
+            llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, openai_api_key=openai_key)
             qa_chain = RetrievalQA.from_chain_type(llm, retriever=db.as_retriever())
             result = qa_chain({"query": question})
             st.success(result["result"])
